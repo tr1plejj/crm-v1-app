caad68e60567994b69443cbdc9103e2d3f415d12
avg_prog_enj <01iVeR@inbox.ru>
2024-01-06 16:44:13 +0500

    created together.py, but still there's adminpanel.py and main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 #6095405341:AAGVEIaNq0i6qdISCC2VtM3r3aExJN0jwQI
 #https://api.telegram.org/bot6095405341:AAGVEIaNq0i6qdISCC2VtM3r3aExJN0jwQI/getChat?chat_id=@fastsellandresell
-
 import telebot
+from adminpanel import Admin
+class All(Admin):
+    def all_product(self):
+        name = self.name.text()
+        price = self.price.text()
+        desc = self.description.text()
+        @bot.message_handler(commands=['send'])
+        def message_in_channel(message):
+            bot.send_message(-1002112682526, name)
 
 TOKEN = '6095405341:AAGVEIaNq0i6qdISCC2VtM3r3aExJN0jwQI'
 
 bot = telebot.TeleBot(TOKEN)
-
-@bot.message_handler(commands=['send'])
-def message_in_channel(message):
-    bot.send_message(-1002112682526, 'im here')
 
 bot.infinity_polling()
---
+++ together.py
@@ -0,0 +1,26 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+import admin
+import telebot
+TOKEN = '6095405341:AAGVEIaNq0i6qdISCC2VtM3r3aExJN0jwQI'
+bot = telebot.TeleBot(TOKEN)
+
+class Admin(QtWidgets.QMainWindow, admin.Ui_Dialog):
+    def __init__(self, parent=None):
+        super(Admin, self).__init__(parent)
+        self.setupUi(self)
+        self.send_button.clicked.connect(self.message_in_channel)
+
+    @bot.message_handler()
+    def message_in_channel(self, message):
+        name = self.name.text()
+        price = self.price.text()
+        desc = self.description.text()
+        bot.send_message(-1002112682526, name)
+
+if __name__ == '__main__':
+    bot.polling()
+    app = QtWidgets.QApplication(sys.argv)
+    a = Admin()
+    a.show()
+    app.exec_()
