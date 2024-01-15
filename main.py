1a28695cc1e81aa6eb81ff22809c623868c64829
avg_prog_enj <01iVeR@inbox.ru>
2024-01-15 20:57:26 +0500

    added an image logic into main.py

--- main.py
+++ main.py
@@ -6,20 +6,26 @@ import telebot
 
 TOKEN = '6095405341:AAGVEIaNq0i6qdISCC2VtM3r3aExJN0jwQI'
 bot = telebot.TeleBot(TOKEN)
-bot.polling()
 class Admin(QtWidgets.QMainWindow, admin.Ui_Dialog):
     def __init__(self, parent=None):
         super(Admin, self).__init__(parent)
         self.setupUi(self)
+        self.load_pic_button.clicked.connect(self.add_image)
         self.send_button.clicked.connect(self.message_in_channel)
 
+    def add_image(self):
+        pic = self.pic_path.text()
+        self.label.setPixmap(QtGui.QPixmap(pic))
     @bot.message_handler()
     def message_in_channel(self, message):
         name = self.name.text()
         price = self.price.text()
         desc = self.description.text()
+        pic = self.pic_path.text()
+        img = open(pic, 'rb')
+        bot.send_photo(-1002112682526, img)
         bot.send_message(-1002112682526, f'Товар: {name}\nЦена: {price}\nОписание: {desc}')
-
+bot.polling()
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     a = Admin()
